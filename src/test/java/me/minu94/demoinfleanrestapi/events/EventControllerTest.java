971e7270262e3a5ff628ad8b010be601e94d304e
Minwoo-Kang <sheepbomb@naver.com>
2019-03-24 11:03:12 +0900

    "fail on unknown properties"

--- src/test/java/me/minu94/demoinfleanrestapi/events/EventControllerTest.java
+++ src/test/java/me/minu94/demoinfleanrestapi/events/EventControllerTest.java
@@ -36,8 +36,37 @@ public class EventControllerTest {
     ObjectMapper objectMapper;
 
     @Test
-    public void createEvent() throws Exception {
+    public void createEvent_badRequest() throws Exception {
+        Event event = Event.builder()
+                .id(100)
+                .name("Spring")
+                .description("REST API Development with Srping")
+                .beginEnrollmentDateTime(LocalDateTime.of(2018 , 11, 23, 14 , 21))
+                .closeEnrollmentDateTime(LocalDateTime.of(2018, 11, 24, 14 ,21))
+                .beginEventDateTime(LocalDateTime.of(2018 , 11, 25, 14, 21))
+                .endEventDateTime(LocalDateTime.of(2018,11,26,14,21))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남역 D2 스타텁 팩토리")
+                .free(true)
+                .offline(false)
+                .build();
 
+
+        mockMvc.perform(post("/api/events")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .accept(MediaTypes.HAL_JSON)
+                .content(objectMapper.writeValueAsString(event)))
+                .andDo(print()) //어떤 요청과 응답이 왔는지 print됨
+                .andExpect(status().isBadRequest())
+                ;
+        //isCreated == 201 response
+
+    }
+
+    @Test
+    public void createEvent() throws Exception {
         Event event = Event.builder()
                 .id(100)
                 .name("Spring")
@@ -64,7 +93,6 @@ public class EventControllerTest {
                 .andExpect(jsonPath("id").exists())
                 .andExpect(jsonPath("id").value(Matchers.not(100)))
                 .andExpect(jsonPath("free").value(Matchers.not(true)));
-
         //isCreated == 201 response
 
     }
